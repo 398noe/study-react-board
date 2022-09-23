4b6026290fe40f385cbc6e54277d9c4e88eb2cda
Sakyuba Noe <398noe@gmail.com>
2022-09-23 16:02:01 +0900

    Add Threads and Posts Page

---
+++ src/pages/Threads.tsx
@@ -0,0 +1,10 @@
+/**
+ * スレッドの一覧を表示するページ
+ */
+export const Threads = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Threads;
---
+++ src/pages/threads/Posts.tsx
@@ -0,0 +1,10 @@
+/**
+ * 掲示板スレッド内を表示するアプリ
+ */
+export const Posts = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Posts;
