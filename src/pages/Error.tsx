333de7d8d025ef98790183f9cc2ea9538a28fab0
Sakyuba Noe <398noe@gmail.com>
2022-09-23 16:47:55 +0900

    Add Error page

---
+++ src/pages/Error.tsx
@@ -0,0 +1,15 @@
+export const Error = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Error!</h1>
+                    <p className="py-6">なにかエラーが起きたみたいです…(´；ω；｀)</p>
+                    <a className="btn btn-primary" href="/">トップに戻る</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Error;
