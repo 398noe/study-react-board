6133278c9f39aad023ee5ca961ba2c01fc934d40
Sakyuba Noe <398noe@gmail.com>
2022-09-23 14:20:39 +0900

    Add linter rules and format

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  overrides: [
+
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  "ignorePatterns": [
+    ".eslintrc.js"
+  ],
+  rules: {
+    'no-use-before-define': "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    'import/prefer-default-export': "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": [
+          "**/*.test.tsx", "**/*setupTests.ts"
+        ]
+      }
+    ],
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'no-void': [
+      'error',
+      {
+        allowAsStatement: true,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      },
+    },
+    'react': {
+      'version': 'detect'
+    },
+  },
+}
