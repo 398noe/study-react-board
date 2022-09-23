4e58926227d2b11d6164370687421bbedc37e052
Sakyuba Noe <398noe@gmail.com>
2022-09-23 16:02:10 +0900

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl" href="/">04Board</a>
+            </div>
+            <div className="flex-none">
+                <div className="btn btn-primary">Button</div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
