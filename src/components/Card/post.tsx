3ce67a825a623de693677594b74e2da921e5641e
Sakyuba Noe <398noe@gmail.com>
2022-09-29 02:12:10 +0900

    Make Tweet Embed

--- src/components/Card/post.tsx
+++ src/components/Card/post.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import { FaFireAlt } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
 import { post as postType } from "../../../types/posts";
+import Twitter from "../Embed/twitter";
 
 type postCardProps = postType;
 export const PostCard: React.FC<postCardProps> = (props: postCardProps) => {
     const { id, post } = props;
 
     const [emoji, setEmoji] = useState<string[]>();
+    const [tweets, setTweets] = useState<string[]>(["1536325209627582464","1300805849607073797"]);
+    const [youTube, setYouTube] = useState<string[]>();
     const [showPost, setShowPost] = useState<string[]>();
 
     return (
@@ -16,13 +19,19 @@ export const PostCard: React.FC<postCardProps> = (props: postCardProps) => {
                 {
                     post.split("\n").map((v) => {
                         return (
-                            <p className="text-lg font-bold" key={v}>{v}</p>
+                            <p className="text-lg font-bold break-all" key={v}>{v}</p>
                         )
                     })
                 }
             </div>
-            <div className="embed">
-                <p>Iframeコンテンツ</p>
+            <div className="embed flex flex-wrap gap-4 justify-center">
+                {
+                    tweets.map((v) => {
+                        return (
+                            <Twitter tweetId={v}/>
+                        )
+                    })
+                }
             </div>
             <div className="divider m-0" />
             <div className="heat">
---
+++ src/components/Embed/twitter.tsx
@@ -0,0 +1,18 @@
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
+interface TwitterProps {
+    tweetId: string;
+}
+
+export const Twitter: React.FC<TwitterProps> = (props: TwitterProps) => {
+    const { tweetId } = props;
+    return (
+        <div className="w-full max-w-xs">
+            <TwitterTweetEmbed
+                tweetId={tweetId}
+            />
+        </div>
+    );
+}
+
+export default Twitter;
