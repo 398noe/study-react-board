2680151d2816ae01f5ffe21c320ea66132e98443
Sakyuba Noe <398noe@gmail.com>
2022-09-28 21:19:37 +0900

    Change fire color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
     return (
         <div className="navbar bg-base-100 shadow-md fixed top-0 z-50">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl" href="/"><span className="pr-2">推しボード</span><FaFireAlt /></a>
+                <a className="btn btn-ghost normal-case text-xl" href="/"><span className="pr-2">推しボード</span><FaFireAlt className="text-rose-600"/></a>
             </div>
             <div className="flex-none">
                 <a className="btn btn-ghost normal-case text-xl" href="/new"><span className="pr-2">新規作成</span><FaRegPlusSquare /></a>
