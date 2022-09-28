743cd5885862421eec23060b7b31a053c2af27a2
Sakyuba Noe <398noe@gmail.com>
2022-09-29 01:24:25 +0900

    Add link for threads to thread

--- src/components/Card/thread.tsx
+++ src/components/Card/thread.tsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import { thread } from "../../../types/threads";
 
 type threadCardProps = thread;
 export const ThreadCard: React.FC<threadCardProps> = (props: threadCardProps) => {
     const { id, title } = props;
     return (
-        <div className="w-full border-2 border-b-indigo-500 shadow-md rounded-md p-4 hover:border-rose-600">
-            <div className="title text-lg font-bold">
-                <p>{title}</p>
+        <Link to={`thread/${id}`}>
+            <div className="w-full border-2 border-b-indigo-500 shadow-md rounded-md p-4 hover:border-rose-600">
+                <div className="title text-lg font-bold">
+                    <p>{title}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
