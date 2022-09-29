a561a38f65609ca4735ba489cc710be283480845
Sakyuba Noe <398noe@gmail.com>
2022-09-29 14:52:04 +0900

    Make anker if it's url

--- src/components/Card/post.tsx
+++ src/components/Card/post.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import reactStringReplace from "react-string-replace";
 import { FaFireAlt } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
 import { post as postType } from "../../../types/posts";
+import { urlRegExp } from "../../utils/url";
 import Twitter from "../Embed/Twitter"; // default宣言しているのでTwitterでも通るけど、宣言先のTWEmbedにするべきかどうか
 import YouTube from "../Embed/YouTube";
 
@@ -10,8 +12,8 @@ export const PostCard: React.FC<postCardProps> = (props: postCardProps) => {
     const { id, post } = props;
 
     const [emoji, setEmoji] = useState<string[]>();
-    const [tweets, setTweets] = useState<string[]>(["1536325209627582464","1300805849607073797"]);
-    const [youTube, setYouTube] = useState<string[]>(["2SciSwfRz2E"]);
+    const [tweets, setTweets] = useState<string[]>(["1536325209627582464", "1300805849607073797"]);
+    const [youTube, setYouTube] = useState<string[]>(["2SciSwfRz2E","zjcuy37uoRM"]);
     const [showPost, setShowPost] = useState<string[]>();
 
     return (
@@ -19,20 +21,31 @@ export const PostCard: React.FC<postCardProps> = (props: postCardProps) => {
             <div className="post">
                 {
                     post.split("\n").map((v) => {
+                        // 更にURLが検出されたらアンカーリンクにする
                         return (
-                            <p className="text-lg font-bold break-all" key={v}>{v}</p>
+                            <p className="text-lg font-bold break-all" key={v}>
+                                {
+                                    reactStringReplace(v, urlRegExp, (match, i) => {
+                                        return (
+                                            <a href={match} className="link-primary">{match}</a>
+                                        )
+                                    })
+                                }
+                            </p>
                         )
                     })
                 }
             </div>
-            <div className="embed flex flex-wrap gap-4 justify-center">
+            <div className="embed flex flex-wrap gap-4 justify-center justify-items-center">
                 {
                     tweets.map((v) => {
                         return (
-                            <Twitter tweetId={v}/>
+                            <Twitter tweetId={v} />
                         )
                     })
                 }
+            </div>
+            <div className="embed flex flex-wrap gap-4 justify-center justify-items-center">
                 {
                     youTube.map((v) => {
                         return (
