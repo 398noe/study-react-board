db28d4ae276f5651e05df412f39c741803a5facb
Sakyuba Noe <398noe@gmail.com>
2022-09-29 14:51:12 +0900

    Changed name

--- utils/utl.ts
+++
@@ -1,6 +0,0 @@
-const urlRegExp = new RegExp(/(https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+)/g);
-
-export const detectURL = (str: string): Array<string> | undefined => {
-    const result =  str.match(urlRegExp);
-    return result?.flat();
-};
