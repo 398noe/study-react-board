019ddadf2c14e7579d758fb566b78fc75f7489e6
Sakyuba Noe <398noe@gmail.com>
2022-10-01 20:46:40 +0900

    Make apiclient with error response status

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,5 +1,39 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import aspida from "@aspida/axios";
 import api from "../api/$api";
 
-export const apiClient = api(aspida(axios, { baseURL: process.env.REACT_APP_BASE_URL}))
+export const apiClient = api(aspida(axios, {
+    baseURL: process.env.REACT_APP_BASE_URL,
+    timeout: 10000
+}));
+
+/**
+ * Errorhandling
+ */
+axios.interceptors.request.use(response => {
+    return response;
+}, error => {
+    if (error instanceof AxiosError) {
+        if (error.response) {
+            switch (error.response.status) {
+                case 400:
+                    // Validation Error
+                    console.error(error.response.data);
+                    break;
+                case 404:
+                    // Not Exist
+                    console.error(error.response.data);
+                    break;
+                case 500:
+                    // Internal Server Error
+                    console.error(error.response.data);
+                    break;
+                default:
+                    console.error("Any other axios error has occured.");
+                    break;
+            }
+        }
+        // When it's axios error
+    }
+    return Promise.reject(error);
+});
