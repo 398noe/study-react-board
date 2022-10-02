9f94c2fd8da5ea7649a7b6bf41f20e4b5626e1e6
Sakyuba Noe <398noe@gmail.com>
2022-10-03 01:55:51 +0900

    Switch if error exists

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router-dom";
+import { errors } from "../store/errors";
+import { useSelector } from "../store/store";
+import Error from "./Error";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export const Layout = () => {
+    // Error Messageを表示するかどうか判定
+    const errorsMessage = useSelector(errors);
+
     return (
         <>
             <Navbar />
             <main className="py-16">
+                {
+                    errorsMessage.ErrorCode ? (
+                        <Error />
+                    ) : (
+                        // eslint-disable-next-line react/jsx-no-useless-fragment
+                        <></>
+                    )
+                }
                 <Outlet />
             </main>
             <Footer />
