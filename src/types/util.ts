0c168db582208a5f254b2638c7cd0beaa3f273fa
Sakyuba Noe <398noe@gmail.com>
2022-10-03 01:29:06 +0900

    Add type for http response

--- src/types/util.ts
+++ src/types/util.ts
@@ -4,10 +4,19 @@ export type parameters = {
     body: {}
 }
 
-export type response400 = {
-    ErrorCode: number,
-    ErrorMessageJP: string,
-    ErrorMessageEN: string
+export type responseError = {
+    ErrorCode: number | null,
+    ErrorMessageJP: string | null,
+    ErrorMessageEN: string | null
 }
 
-export type response500 = response400;
+export type response400 = responseError & {
+    ErrorCode: 400
+}
+
+export type response404 = responseError & {
+    ErrorCode: 404
+}
+export type response500 = responseError & {
+    ErrorCode: 500
+};
