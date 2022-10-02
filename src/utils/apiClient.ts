df9aff902eb52ac011e60bd29116ce88f0f4eec1
Sakyuba Noe <398noe@gmail.com>
2022-10-03 01:27:59 +0900

    Change function name

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosError } from "axios";
 import aspida from "@aspida/axios";
 import api from "../api/$api";
+import { store } from "../store/store";
+import { setErrors } from "../store/errors";
+import { response400Data, response404Data, response500Data } from "./errorRespnose";
 
 export const apiClient = api(aspida(axios, {
     baseURL: process.env.REACT_APP_BASE_URL,
@@ -15,18 +18,20 @@ axios.interceptors.request.use(response => {
 }, error => {
     if (error instanceof AxiosError) {
         if (error.response) {
+            console.error(error.response.data);
             switch (error.response.status) {
                 case 400:
                     // Validation Error
-                    console.error(error.response.data);
+                    // response400 | response404 | response500型のデータを渡してあげる
+                    store.dispatch(setErrors(response400Data));
                     break;
                 case 404:
                     // Not Exist
-                    console.error(error.response.data);
+                    store.dispatch(setErrors(response404Data));
                     break;
                 case 500:
                     // Internal Server Error
-                    console.error(error.response.data);
+                    store.dispatch(setErrors(response500Data));
                     break;
                 default:
                     console.error("Any other axios error has occured.");
