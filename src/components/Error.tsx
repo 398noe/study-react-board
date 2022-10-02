693088d60401e245143e4326780c4e6f0d7ec8c9
Sakyuba Noe <398noe@gmail.com>
2022-10-03 01:55:57 +0900

    Add error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,23 @@
+import { RiCloseCircleLine } from "react-icons/ri";
+import { errors } from "../store/errors";
+import { useSelector } from "../store/store";
+
+export const Error = () => {
+    // Error Messageを表示
+    const errorsMessage = useSelector(errors);
+
+    return (
+        <div className="container mx-auto p-4">
+            <div className="flex flex-col justify-center">
+                <div className="alert alert-error shadow-lg">
+                    <div>
+                        <RiCloseCircleLine className="text-xl" />
+                        <span>{errorsMessage.ErrorMessageJP}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Error;
