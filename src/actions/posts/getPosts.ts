8f722e5ddaf6e6fa006b66659bf6c4bd0e5a11bf
Sakyuba Noe <398noe@gmail.com>
2022-10-03 14:54:52 +0900

    Add getPosts for action

---
+++ src/actions/posts/getPosts.ts
@@ -0,0 +1,22 @@
+import { AxiosError } from "axios";
+import { postsGetParameters } from "../../types/posts";
+import { apiClient } from "../../utils/apiClient";
+
+export const getPosts = async (parameters: postsGetParameters) => {
+    const { path, query } = parameters;
+    try {
+        // eslint-disable-next-line no-underscore-dangle
+        const threadsGetResponse = await apiClient.threads._threadId(path.threadId).posts.get({
+            query
+        })
+        return threadsGetResponse.body;
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            /**
+             * @link https://zenn.dev/yumemi_inc/articles/2022-02-13-ts-type-safe-request
+             * @todo Error Messageの保存については共通のエラーハンドリングを実装済み
+             */
+        }
+        throw error;
+    }
+}
