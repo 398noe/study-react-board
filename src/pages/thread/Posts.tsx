5382fb1f330c431f3d4d5862f40753e459ad91c4
Sakyuba Noe <398noe@gmail.com>
2022-10-04 01:46:36 +0900

    Add postPosts

--- src/pages/thread/Posts.tsx
+++ src/pages/thread/Posts.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { postsGetParameters, postsGetResponse200 } from "../../types/posts";
+import { postsGetParameters, postsGetResponse200, postsPostParameters } from "../../types/posts";
 import { PostCard } from "../../components/Card/post";
 import { getPosts } from "../../actions/posts/getPosts";
 import { useSelector } from "../../store/store";
 import { errors } from "../../store/errors";
 import { postsGetResponse200Data } from "../../toy/posts";
 import { getTrueOffset } from "../../utils/offset";
+import { postPosts } from "../../actions/posts/postPosts";
 
 /**
  * 掲示板スレッド内を表示するアプリ
@@ -33,9 +34,51 @@ export const Posts = () => {
         body: {}
     });
 
+    const [postParameters, setPostParameters] = useState<postsPostParameters>({
+        path: {
+            threadId: threadId ?? "0"
+        },
+        query: {
+        },
+        body: {
+            post: ""
+        }
+    });
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setNewPost(e.target.value);
+    }
+
+    useEffect(() => {
+        setPostParameters({
+            ...postParameters, body: {
+                post: newPost
+            }
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [newPost]);
+
+    const createNewPost = async () => {
+        // postする
+        try {
+            const postsPostReponse = await postPosts(postParameters);
+            console.log(postsPostReponse);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+        // 再度スレッドを取得し直す
+        // ここのロジックは再度利用しているので、共通化しておきたい
+        try {
+            const postsGetResponse = await getPosts(getParameters);
+            console.log(postsGetResponse);
+            setPosts(postsGetResponse);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+        // newpostをリセット
+        setNewPost("");
     }
 
     /**
@@ -48,7 +91,7 @@ export const Posts = () => {
             if (newOffset < 1) {
                 return 1;
             }
-            return newOffset
+            return newOffset;
         });
     }
 
@@ -132,7 +175,7 @@ export const Posts = () => {
                                 <div className="flex flex-col gap-4 w-full md:w-1/3">
                                     <p className="p-4 text-2xl font-bold">投稿する</p>
                                     <textarea className="textarea textarea-bordered w-full h-48" placeholder="Write comment" value={newPost} onChange={handleChange} />
-                                    <div className="btn">投稿！</div>
+                                    <div className="btn" onClick={() => {createNewPost()}} onKeyDown={() => {createNewPost()}}>投稿！</div>
                                 </div>
                             )
                     }
