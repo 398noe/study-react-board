bf4be8999aa10674a369536ef2d9003c8e564e3a
Sakyuba Noe <398noe@gmail.com>
2022-10-03 15:06:54 +0900

    Disable post compoennt when got 404 error

--- src/pages/thread/Posts.tsx
+++ src/pages/thread/Posts.tsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { postsGetParameters, postsGetResponse200 } from "../../types/posts";
 import { PostCard } from "../../components/Card/post";
-import { postsGetResponse200Data } from "../../toy/posts";
 import { getPosts } from "../../actions/posts/getPosts";
+import { useSelector } from "../../store/store";
+import { errors } from "../../store/errors";
 
 /**
  * 掲示板スレッド内を表示するアプリ
  */
 export const Posts = () => {
-    const [posts, setPosts] = useState<postsGetResponse200>(postsGetResponse200Data);
+    const errorsMessage = useSelector(errors);
+    // 投稿記事
+    const [posts, setPosts] = useState<postsGetResponse200>();
 
     // 新規投稿する文章
     const [newPost, setNewPost] = useState<string>("");
@@ -53,23 +56,47 @@ export const Posts = () => {
             <div className="container mx-auto p-4">
                 <div className="flex gap-4 flex-wrap md:flex-nowrap">
                     <div className="flex flex-col gap-4 w-full md:w-2/3">
-                        <p className="p-4 text-2xl font-bold">{posts.title}</p>
+                        <p className="p-4 text-2xl font-bold">{posts?.title}</p>
                         {
-                            posts.posts.map((v) => {
+                            posts?.posts.map((v) => {
                                 return (
                                     <PostCard key={v.id} id={v.id} post={v.post} />
                                 );
                             })
                         }
+                        <div className="container mx-auto p-4">
+                            <div className="form-control">
+                                <div className="flex flex-col gap-8 items-center justify-center">
+                                    <div className="btn-group">
+                                        <div className="btn">1</div>
+                                        <div className="btn btn-disabled">...</div>
+                                        <input className="input input-bordered rounded-none w-16" type="text" />
+                                        <div className="btn">n+1</div>
+                                        <div className="btn">GO</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-                    <div className="flex flex-col gap-4 w-full md:w-1/3">
-                        <p className="p-4 text-2xl font-bold">投稿する</p>
-                        <textarea className="textarea textarea-bordered w-full h-48" placeholder="Write comment" value={newPost} onChange={handleChange}/>
-                        <div className="btn">投稿！</div>
-                    </div>
+                    {
+                        errorsMessage.ErrorCode ?
+                            (
+                                // eslint-disable-next-line react/jsx-no-useless-fragment
+                                <></>
+                            )
+                            : (
+                                <div className="flex flex-col gap-4 w-full md:w-1/3">
+                                    <p className="p-4 text-2xl font-bold">投稿する</p>
+                                    <textarea className="textarea textarea-bordered w-full h-48" placeholder="Write comment" value={newPost} onChange={handleChange} />
+                                    <div className="btn">投稿！</div>
+                                </div>
+                            )
+                    }
+
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
