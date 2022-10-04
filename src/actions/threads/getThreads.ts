117cfaee18e2ec3d46e995c7ab2ddea23139404f
Sakyuba Noe <398noe@gmail.com>
2022-10-04 21:18:24 +0900

    Delete unused comment

--- src/actions/threads/getThreads.ts
+++ src/actions/threads/getThreads.ts
@@ -11,10 +11,6 @@ export const getThreads = async (parameters: threadsGetParameters) => {
         return threadsGetResponse.body;
     } catch (error) {
         if (error instanceof AxiosError) {
-            /**
-             * @link https://zenn.dev/yumemi_inc/articles/2022-02-13-ts-type-safe-request
-             * @todo Error Messageの保存については共通のエラーハンドリングを実装済み
-             */
         }
         throw error;
     }
--- src/actions/threads/postThreads.ts
+++ src/actions/threads/postThreads.ts
@@ -11,10 +11,6 @@ export const postThreads = async (parameters: threadsPostParameters) => {
         return threadsPostResponse.body;
     } catch (error) {
         if (error instanceof AxiosError) {
-            /**
-             * @link https://zenn.dev/yumemi_inc/articles/2022-02-13-ts-type-safe-request
-             * @todo Error Messageの保存については共通のエラーハンドリングを実装済み
-             */
         }
         throw error;
     }
